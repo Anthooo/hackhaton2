52559dba49178daa7b72e2a5cf1e799ae76c2d6a
acrobat22 <acrobat22@gmail.com>
2016-12-15 17:48:41 +0100

    installe exif_read_data controleur challenge show action

--- src/CatchmeBundle/Controller/ChallengeController.php
+++ src/CatchmeBundle/Controller/ChallengeController.php
@@ -51,6 +51,26 @@ class ChallengeController extends Controller
         ));
     }
 
+    // Fonction pour retrouver les coordonnées de l'image
+    public function gps2Num($coordPart) {
+          $parts = explode('/', $coordPart);
+          if (count($parts) <= 0)
+              return 0;
+          if (count($parts) == 1)
+              return $parts[0];
+          return floatval($parts[0]) / floatval($parts[1]);
+    }
+
+    // Fonction pour retrouver les coordonnées de l'image
+    public function getGps($exifCoord, $hemi) {
+          $degrees = count($exifCoord) > 0 ? $this->gps2Num($exifCoord[0]) : 0;
+          $minutes = count($exifCoord) > 1 ? $this->gps2Num($exifCoord[1]) : 0;
+          $seconds = count($exifCoord) > 2 ? $this->gps2Num($exifCoord[2]) : 0;
+          $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+          return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+    }
+
+
     /**
      * Finds and displays a challenge entity.
      *
@@ -59,8 +79,46 @@ class ChallengeController extends Controller
     {
 /*        $deleteForm = $this->createDeleteForm($challenge);*/
 
+    $nomImage = $challenge->getImage()->getUrl();
+    $chemin = ('../web/uploads/images/' . $nomImage);
+    $exif = exif_read_data($chemin);
+    if(array_key_exists('GPSLongitude', $exif) && array_key_exists('GPSLatitude', $exif)) {
+        $lng = $this->getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+        $lat = $this->getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+        $response = array(
+            "status" => "success",
+            "reason" => "",
+            "gps" => array(
+                "lat" => $lat,
+                "lng" => $lng
+            ),
+        );
+    }
+    else {
+        $response = array(
+            "status" => "fail",
+            "reason" => "The uploaded picture does not have GPS information.",
+            "exif" => $exif
+        );
+    }
+
+  // Isole lat et long de la photo
+  $latitudePhoto = ($response['gps']['lat']);
+  $longitudePhoto = ($response['gps']['lng']);
+  // calcul nouvelle lat et long pour centre zone de recherche
+  $randomFloatLat = (rand(0, 1) / 10000);
+  // var_dump(floatval($randomFloatLat));
+  $randomFloatLong = (rand(0, 99) / 10000);
+  // var_dump(floatval($randomFloatLong));
+  $latitudeCentreDecale = ($response['gps']['lat']-$randomFloatLat);
+  $longitudeCentreDecale = ($response['gps']['lng']+$randomFloatLong);
+
+
+
         return $this->render('challenge/show.html.twig', array(
             'challenge' => $challenge,
+            'longitudeCentre' => $longitudeCentreDecale,
+            'latitideCentre' => $latitudeCentreDecale,
 //            'image'=> $challenge->getImages(),
 /*            'delete_form' => $deleteForm->createView(),*/
         ));
